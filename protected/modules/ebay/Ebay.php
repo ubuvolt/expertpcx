715dbbd1058fbe160920814bac721eea96206950
Bartek Wolski <ubuvolt@gmail.com>
2017-11-29 14:04:35 +0000

    get product name from API

--- protected/modules/ebay/Ebay.php
+++ protected/modules/ebay/Ebay.php
@@ -127,7 +127,8 @@ class Ebay {
 //        $return_array['product'] = $product_xml->{'_'};
 
 
-        d::d($return_array['product']);
+//        d::d($return_array['product']);
+         
 
         return $return_array;
     }
