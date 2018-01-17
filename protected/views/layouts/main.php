ed877a90adca2cd7d10b105e6bf2d8d22b3d4eb5
Bartek Wolski <ubuvolt@gmail.com>
2018-01-17 11:57:00 +0000

    eBay API search view

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -29,6 +29,7 @@ $curent_company = AdminCentralStorage::get_central_setting(Yii::app()->user->nam
                         array('label' => 'Login', 'url' => array('/site/login'), 'visible' => Yii::app()->user->isGuest),
                         array('label' => 'Logout (' . Yii::app()->user->name . ')', 'url' => array('/site/logout'), 'visible' => !Yii::app()->user->isGuest),
                         array('label' => 'eBay', 'url' => array('/ebayApi/main')),
+                        array('label' => 'eBaySearch', 'url' => array('/ebaySearch/ebaySearch')),
                     ),
                 ));
                 ?>
