594cb06d2a39d9c28edfd2a9926b95ee3c34aa6e
Bartek Wolski <ubuvolt@gmail.com>
2018-01-22 21:01:57 +0000

    ebay_price_tracking

---
+++ protected/sql/ebay_price_tracking.sql
@@ -0,0 +1,22 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  ubuvolt
+ * Created: 20-Aug-2017
+ */
+
+
+DROP TABLE IF EXISTS `ebay_price_monitor`;
+
+CREATE TABLE `ebay_price_monitor` 
+(
+    `id`            INT NOT NULL AUTO_INCREMENT,
+    `product`       VARCHAR(600),
+    `seller`        VARCHAR(128),
+    `url`           VARCHAR(2048) NOT NULL,
+    `price`         FLOAT(7,2),
+    primary key (`id`)
+) engine=InnoDB DEFAULT CHARSET=utf8;
